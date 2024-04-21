e8ed4ebae9735c8f8ce234c8bd8f20f26f3198f9
jiteshdhamija <jiteshdhamija138@gmail.com>
2024-04-21 19:10:26 +0530

    finally swagger is working

--- app/src/main/java/com/kyaKhaaye/Controllers/RestControllers.java
+++ app/src/main/java/com/kyaKhaaye/Controllers/RestControllers.java
@@ -2,12 +2,12 @@ package com.kyaKhaaye.Controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kyaKhaaye.Models.AllFields;
 
-@Controller
+@RestController
 public class RestControllers {
 
 	@PostMapping("/allRandom")
