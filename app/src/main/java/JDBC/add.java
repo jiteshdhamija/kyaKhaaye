e1e6968fc5f5cf8e3819cec07be3eeff484ecd23
jitesh-jitesh <44140820+jiteshdhamija@users.noreply.github.com>
2024-07-31 01:55:29 +0530

    some controllers and jdbc connection failing due to TCP/IP connection not established

--- app/src/main/java/JDBC/add.java
+++ app/src/main/java/JDBC/add.java
@@ -1,9 +1,24 @@
 package JDBC;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*create TABLE food ( id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+dish varchar(255),
+cuisine varchar(255), 
+beverage boolean, 
+veg boolean,
+course varchar(255),
+hot boolean,
+region varchar(255)); 
+(dish,cuisine,beverage,veg,course,hot,region)*/
 
 public class add {
-    repo r = new repo();
-    Connection c = r.connect();
-
+    public void addToDb(String query) throws SQLException, ClassNotFoundException {
+        repo r = new repo();
+        Connection c = r.connect();
+        Statement st = c.createStatement();
+        st.executeQuery(query);
+    }
 }
